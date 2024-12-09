5efdcbfcd2597c8096e574309d440f09d89b4f5e
Frogithezhrda <omersaban169@gmail.com>
2024-12-09 19:16:28 +0200

    first

---
+++ SafeAndSound/part1.cpp
@@ -0,0 +1,26 @@
+#include "part1.h"
+#include <iostream>
+
+char* string_copy(char* dest, unsigned int destsize, char* src)
+{
+	char* ret = dest;
+	int i = 0;
+	while (i < destsize - 1 && *src) {
+		*dest++ = *src++;
+		i++;
+	}
+	*dest = '\0';
+	return ret;
+}
+
+void part1()
+{
+	char password[] = "secret";
+	char dest[12];
+	char src[] = "hello world!";
+
+	string_copy(dest, 12, src);
+
+	std::cout << src << std::endl;
+	std::cout << dest << std::endl;
+}
